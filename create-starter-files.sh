b4215c0a01111b8e7b6f23eac363e6e6f2a3d626
as00r <as00r.dev@gmail.com>
2022-11-16 00:40:51 +0530

    Fix location address
    updated location

--- create-starter-files.sh
+++ create-starter-files.sh
@@ -4,9 +4,9 @@
 function csf() {
     touch index.html style.css script.js
     echo "index.html,style.css,script.js created."
-    cat ~/repos/html-boilerplate/html-boilerplate.html > ./index.html
+    cat ~/repos/create-starter-files/html-boilerplate.html > ./index.html
     echo "added HTML boilerplate"
-    cat ~/repos/html-boilerplate/css-boilerplate.css > ./style.css
+    cat ~/repos/create-starter-files/css-boilerplate.css > ./style.css
     echo "added CSS boilerplate"
     echo "Finished Successfully. Press Ctrl + C if stuck."
 }
